6f267d998ae7324b8f38208e15eb2b3fb29795fb
rhett <rhettss@icloud.com>
2024-03-24 16:12:21 -0400

    learning ts

--- README.md
+++ README.md
@@ -9,3 +9,8 @@
 
 npm run vscode:prepublish
 then fn F5
+
+
+getChildren is called first, when the Tree View is opened. it returns back a list of children to display. 
+initailly, the root element will be null. but when opening children nodes later, it will not be null
+
--- src/fileExplorer.ts
+++ src/fileExplorer.ts
@@ -136,6 +136,7 @@ export class FileStat implements vscode.FileStat {
 
 interface Entry {
 	uri: vscode.Uri;
+	fileName: String;
 	type: vscode.FileType;
 }
 
@@ -178,9 +179,14 @@ export class FileSystemProvider implements vscode.TreeDataProvider<Entry>, vscod
 		return this._readDirectory(uri);
 	}
 
+	//called by the getChildren function, when root is null. 
 	async _readDirectory(uri: vscode.Uri): Promise<[string, vscode.FileType][]> {
+		console.log("in the _readDirectory method");
+		if (! fs.existsSync(uri.fsPath)) {
+			console.log("does not exist");
+		}
 		const children = await _.readdir(uri.fsPath);
-
+		console.log("past await in _readDirectory");
 		const result: [string, vscode.FileType][] = [];
 		for (let i = 0; i < children.length; i++) {
 			const child = children[i];
@@ -225,53 +231,54 @@ export class FileSystemProvider implements vscode.TreeDataProvider<Entry>, vscod
 	//#endregion
 
 	async getChildren(element?: Entry): Promise<Entry[]> {
-		console.log("debugging. getChildren called");
+		console.log("getChildren called with element = ", element);
 		if (vscode.workspace.workspaceFolders == null)
 			return [];
 
-		//not sure what this is for
+		// for the initial call (root) this will be null, and thus will be false, and and skipped over.
 		if (element) {
 			console.log("getting kids of " + element.uri.toString());
 			var folderuri = vscode.Uri.parse(element.uri.toString().substring(0,element.uri.toString().lastIndexOf(".")) + "-tests");
 			console.log(folderuri.toString());
 			try
 			{
+				console.log("in try");
 				const children = await this.readDirectory(folderuri);
-				
+				console.log("past awaiting");
 				console.log("getting offspring:" + children.toString());
 				console.log("folder:" + folderuri.toString());
 				console.log("as file:" + vscode.Uri.file(folderuri.toString()));
 				console.log("as fs:" + folderuri.fsPath);
 			
-				return children.map(([name, type]) => ({ uri: Utils.joinPath(folderuri, name), type }));
+				return children.map(([name, type]) => ({ uri: Utils.joinPath(folderuri, name),fileName: name, type }));
 				}
 			catch(e)
 			{
 				console.log("Error attempting to open test folder: " + e.message);
 				return [];
 			}
+		} else {
+
+			const workspaceFolder = vscode.workspace.workspaceFolders.filter(folder => folder.uri.scheme === 'file')[0];
+			console.log(workspaceFolder)
+			if (workspaceFolder) {
+				const directoryItems = await this.readDirectory(workspaceFolder.uri);
+				const children = directoryItems.filter(e => e[0].endsWith(".dmn"))
+
+				
+				children.sort((a, b) => {
+					if (a[1] === b[1]) {
+						return a[0].localeCompare(b[0]);
+					} else {
+						return a[1] === vscode.FileType.Directory ? -1 : 1;
+					}
+				});
+				console.log("leaving getChildren. count children:", children.length)
+				return children.map(([name, type]) => ({ uri: vscode.Uri.file(path.join(workspaceFolder.uri.fsPath, name)), fileName: name, type }));
+			}
+
+			//return [];
 		}
-
-		const workspaceFolder = vscode.workspace.workspaceFolders.filter(folder => folder.uri.scheme === 'file')[0];
-		if (workspaceFolder) {
-			const temparr = await this.readDirectory(workspaceFolder.uri);
-
-			const children = temparr.filter(function( obj ) {
-				return obj[0].endsWith(".dmn");
-			});
-
-			
-			children.sort((a, b) => {
-				if (a[1] === b[1]) {
-					return a[0].localeCompare(b[0]);
-				}
-				return a[1] === vscode.FileType.Directory ? -1 : 1;
-			});
-			console.log("getting ws children of " + children.toString());
-			return children.map(([name, type]) => ({ uri: vscode.Uri.file(path.join(workspaceFolder.uri.fsPath, name)), type }));
-		}
-
-		return [];
 	}
 
 		//a registered command
@@ -285,7 +292,6 @@ export class FileSystemProvider implements vscode.TreeDataProvider<Entry>, vscod
 		//this._onDidChangeTreeData.fire(element);// _onDidChangeFile.fire([{type: vscode.FileChangeType.Changed, uri: element}]);
 		let orange = vscode.window.createOutputChannel(element.uri.toString() + " Test");
 		//Write to output.
-		
 		 
 		let dmnStr: string = '<dmn:definitions xmlns:dmn="http://www.omg.org/spec/DMN/20180521/MODEL/" xmlns="https://kiegroup.org/dmn/_79B69A7F-5A25-4B53-BD6A-3216EDC246ED" xmlns:feel="http://www.omg.org/spec/DMN/20180521/FEEL/" xmlns:kie="http://www.drools.org/kie/dmn/1.2" xmlns:dmndi="http://www.omg.org/spec/DMN/20180521/DMNDI/" xmlns:di="http://www.omg.org/spec/DMN/20180521/DI/" xmlns:dc="http://www.omg.org/spec/DMN/20180521/DC/" id="_E01B9C96-BCD5-4618-AC02-35F77D1065E2" name="loan" typeLanguage="http://www.omg.org/spec/DMN/20180521/FEEL/" namespace="https://kiegroup.org/dmn/_79B69A7F-5A25-4B53-BD6A-3216EDC246ED"> <dmn:extensionElements/> <dmn:itemDefinition id="_94619DCD-2602-4A43-97E9-9809D76F15A5" name="tLoan" isCollection="false"> <dmn:itemComponent id="_3C8953D0-F1AD-40F6-BC5E-813F3185F3F1" name="amount" isCollection="false"> <dmn:typeRef>number</dmn:typeRef> </dmn:itemComponent> <dmn:itemComponent id="_1CF9A4C0-9218-4F96-AB1A-66570002B7C5" name="years" isCollection="false"> <dmn:typeRef>number</dmn:typeRef> </dmn:itemComponent> </dmn:itemDefinition> <dmn:inputData id="_058269A1-A5AC-44CB-9479-16A04DC19D77" name="Credit score"> <dmn:extensionElements/> <dmn:variable id="_BA408840-4504-44EB-BD7F-6261F787E342" name="Credit score" typeRef="number"/> </dmn:inputData> <dmn:inputData id="_65226EA1-EE4A-41F1-91A2-B2A940A75982" name="Salary"> <dmn:extensionElements/> <dmn:variable id="_204FE05E-BCF8-4AF5-AD36-B5A04E0EB929" name="Salary" typeRef="number"/> </dmn:inputData> <dmn:inputData id="_516E0AEC-03F3-43F4-B886-C489CA82A1C9" name="Loan"> <dmn:extensionElements/> <dmn:variable id="_90456001-E9FA-4DDC-B2BA-DF1B62A25AAA" name="Loan" typeRef="tLoan"/> </dmn:inputData> <dmn:decision id="_6A3FDF72-7F17-4DC5-AC1D-1DCC972C0146" name="Preapproval"> <dmn:extensionElements/> <dmn:variable id="_2C06D150-7AB2-48C9-BFC6-F48884FD96EF" name="Preapproval" typeRef="boolean"/> <dmn:informationRequirement id="_9BFEFD1E-9830-4630-8171-B6F1D3100308"> <dmn:requiredInput href="#_058269A1-A5AC-44CB-9479-16A04DC19D77"/> </dmn:informationRequirement> <dmn:informationRequirement id="_4736D6D5-5A61-4C0E-ADBA-5AAD99221445"> <dmn:requiredDecision href="#_50635164-1A27-4B84-AE16-639A118CE44C"/> </dmn:informationRequirement> <dmn:decisionTable id="_E7994A2B-1189-4BE5-9382-891D48E87D47" hitPolicy="UNIQUE" preferredOrientation="Rule-as-Row"> <dmn:input id="_9CCC5EBB-BC59-4397-B478-BB434279EBF8"> <dmn:inputExpression id="_85A80C30-68FA-405F-BE6D-D1B6C484CD81" typeRef="number"> <dmn:text>Credit score</dmn:text> </dmn:inputExpression> </dmn:input> <dmn:input id="_D160037A-3B50-44FE-BC28-700F750D1A3A"> <dmn:inputExpression id="_2774CDB9-0B9D-4B89-9639-5D1FD7D6D61B" typeRef="number"> <dmn:text>DTI</dmn:text> </dmn:inputExpression> </dmn:input> <dmn:output id="_8220E87A-3913-4FC5-9926-4A9FD28F82EA"/> <dmn:annotation name="annotation-1"/> <dmn:rule id="_C09BF27F-E03C-4390-8719-E7411784ABCB"> <dmn:inputEntry id="_C02D6987-5B78-42ED-B48D-E3CE5844C22B"> <dmn:text>&lt;700</dmn:text> </dmn:inputEntry> <dmn:inputEntry id="_922FFAE0-9635-420B-8002-4583065E6710"> <dmn:text>&lt;=.28</dmn:text> </dmn:inputEntry> <dmn:outputEntry id="_75A30858-95A6-46F4-89D5-BFFF8B3739AD"> <dmn:text>true</dmn:text> </dmn:outputEntry> <dmn:annotationEntry> <dmn:text/> </dmn:annotationEntry> </dmn:rule> <dmn:rule id="_5CE22529-735A-43D8-9043-E342F58D1CDD"> <dmn:inputEntry id="_14C78BCF-736B-4032-8AB8-B73967479EEF"> <dmn:text>&gt;=700</dmn:text> </dmn:inputEntry> <dmn:inputEntry id="_6AA1D822-C08E-4B4E-BE8B-4C3587E6F034"> <dmn:text>-</dmn:text> </dmn:inputEntry> <dmn:outputEntry id="_CD1AFB69-DDC6-416E-B780-FE2FB3AAC6C1"> <dmn:text>true</dmn:text> </dmn:outputEntry> <dmn:annotationEntry> <dmn:text/> </dmn:annotationEntry> </dmn:rule> <dmn:rule id="_5EB4DDC3-CE49-419A-B7C8-7A6916D6334F"> <dmn:inputEntry id="_2F5D6384-4AE6-4408-88F4-4D3C86F26649"> <dmn:text>&lt;700</dmn:text> </dmn:inputEntry> <dmn:inputEntry id="_5CA8DA56-07BE-4410-85DD-800508300DB8"> <dmn:text>&gt;.28</dmn:text> </dmn:inputEntry> <dmn:outputEntry id="_8B85A627-73DD-46E7-99DA-26499B3DD9BD"> <dmn:text>false</dmn:text> </dmn:outputEntry> <dmn:annotationEntry> <dmn:text/> </dmn:annotationEntry> </dmn:rule> </dmn:decisionTable> </dmn:decision> <dmn:decision id="_50635164-1A27-4B84-AE16-639A118CE44C" name="DTI"> <dmn:extensionElements/> <dmn:variable id="_7FF18790-80C8-4124-9BFB-93383CE6A50F" name="DTI" typeRef="number"/> <dmn:informationRequirement id="_7A538A2F-562E-4E49-B5F0-572FFCFEF4CB"> <dmn:requiredInput href="#_516E0AEC-03F3-43F4-B886-C489CA82A1C9"/> </dmn:informationRequirement> <dmn:informationRequirement id="_E508941C-2DE2-41DD-8406-EA8AD646DB7F"> <dmn:requiredInput href="#_65226EA1-EE4A-41F1-91A2-B2A940A75982"/> </dmn:informationRequirement> <dmn:literalExpression id="_60A349DD-1F30-488B-BA65-74160F6496F3"> <dmn:text>(Loan.amount / Loan.years)/Salary</dmn:text> </dmn:literalExpression> </dmn:decision> </dmn:definitions>';
  
@@ -406,6 +412,7 @@ export class FileSystemProvider implements vscode.TreeDataProvider<Entry>, vscod
 	}
 
 	async getTreeItem(element: Entry): Promise<vscode.TreeItem> {
+		console.log("getting element of type Entry:", element)
 		const treeItem = new vscode.TreeItem(element.uri, element.type === vscode.FileType.Directory ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None);
 
 		var size: number = 0;
